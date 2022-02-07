8aaf99483cf8a162a35cd18ae574d18535009a4d
Hannah Lim <hjeong.lim@gmail.com>
2022-02-06 21:33:50 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import item from './data';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Menu App</h2>
+    </div>
+  );
+}
+
+export default App;
